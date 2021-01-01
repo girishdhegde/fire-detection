80beace73939785cf9325ec9dfd98d53b31c657b
girish dattatray hegde <60293679+girishdhegde@users.noreply.github.com>
2021-01-01 09:51:47 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -23,27 +23,36 @@ def viz(img, pred, classes=None, colors=None):
                         #                     tuple(pt1), fontFace=0, fontScale=.5, color=(0, 255, 255))
     return img
 
-def detect(img, net, ):
+def detect(img, net, conf_th=.2, iou_th=0.5, S=7, B=2, C=2):
+    '''
+    img: RGB image 
+    net: trained yolo model
+    conf_th: Confidence threshold for nms
+    iou_th: iou threshold for nms
+    '''
     m, n, *_ = img.shape
     img = cv2.resize(img, (448, 448), interpolation=cv2.INTER_CUBIC)
     with torch.no_grad():
         out = net(torch.tensor(img).permute(2, 0, 1).unsqueeze(0).float())
-        out = nms(out, conf_th=.2, iou_th=0.2, S=7, B=2, C=2)
+        out = nms(out, conf_th=conf_th, iou_th=iou_th, S=S, B=B, C=C)
         img = viz(img, out[0])
     return cv2.resize(img, (m, n), interpolation=cv2.INTER_CUBIC)
 
 if __name__ == '__main__':
     from sys
-#     from model import yolo
+    # from model import yolo
     from yolo_resnet import yolo
+
     ''' argv: predict.py   imag_path   pretrained_weight_path'''
     path = sys.argv[1]
     load = sys.argv[2]
-    net = yolo()
+    # C: No. classes, 2 for fire-smoke, 80 for coco etc
+    C = 80
+    net = yolo(C=C)
     net.load_state_dict(torch.load(load))
     net.eval()
     img = cv2.imread(path)
-    out = detect(img, net)
+    out = detect(img, net, conf_th=0.2, C=C)
     cv2.imshow('img', out)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
